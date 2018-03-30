d7cfa3a9002eef980d0954948677f22dd7d463e9
Imran <imranvitt@gmail.com>
2018-03-30 23:40:47 +0530

    Initial Commit

---
+++ app/src/main/java/client/foodpark/com/foodparkclient/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package client.foodpark.com.foodparkclient.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import client.foodpark.com.foodparkclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
